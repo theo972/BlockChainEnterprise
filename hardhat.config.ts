4c83ad2543b704adbdb360a733621ec642263d66
Theovady Moutty <theomoutty@gmail.com>
2023-12-01 17:47:08 +0100

    feat: hardhat config verifiy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "dotenv/config"
 import "@nomicfoundation/hardhat-toolbox";
 
-import "@nomiclabs/hardhat-etherscan";
-
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY!],
     },
   },
   etherscan: {
-    apiKey: process.env.PRIVATE_KEY_ETH,
+    apiKey: process.env.PRIVATE_KEY_ETH
   },
 };
 
