dffc4947c30bfdf6fbcaf23e36a61bbb8fdfe2ad
PRAALY-DEV <kametobot@gmail.com>
2023-12-01 16:57:21 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,16 +2,19 @@ import { HardhatUserConfig } from "hardhat/config";
 import "dotenv/config"
 import "@nomicfoundation/hardhat-toolbox";
 
+import "@nomiclabs/hardhat-etherscan";
 
-//
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY!],
     },
   },
+  etherscan: {
+    apiKey: process.env.PRIVATE_KEY_ETH,
+  },
 };
+
 
 export default config;
