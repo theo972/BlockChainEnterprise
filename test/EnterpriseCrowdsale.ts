215797b2fdd5b8a913da077ad59efd8168e8d83d
Theovady Moutty <theomoutty@gmail.com>
2023-12-04 10:59:17 +0100

    feat: test should not allow purchasing tokens if not enough tokens available

--- test/EnterpriseCrowdsale.ts
+++ test/EnterpriseCrowdsale.ts
@@ -25,4 +25,27 @@ describe("EnterpriseCrowdsale", function () {
 
         expect(buyerBalance).to.equal(1);
     });
+
+    it("should allow purchasing tokens if Not enough tokens available", async function () {
+        const [owner, otherAccount] = await ethers.getSigners();
+
+        const EnterpriseToken = await ethers.getContractFactory("EnterpriseToken");
+        const enterpriseToken = await EnterpriseToken.deploy(100000);
+        await enterpriseToken.waitForDeployment();
+
+        const EnterpriseCrowdsale = await ethers.getContractFactory("EnterpriseCrowdsale");
+        const enterpriseCrowdsale = await EnterpriseCrowdsale.deploy(
+        await enterpriseToken.getAddress(),
+        1,
+        100,
+        10000 
+        );
+    
+        await enterpriseToken.transfer(await enterpriseCrowdsale.getAddress(), 200);    
+        await owner.sendTransaction({ to: otherAccount.address, value: ethers.parseEther("1000") });
+        
+        await expect(
+            enterpriseCrowdsale.connect(otherAccount).purchaseTokens(400, { value: ethers.parseEther("200") })
+        ).to.be.revertedWith('Not enough tokens available');   
+    });
 });
