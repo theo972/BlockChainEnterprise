7313059cfbecae74bc70a103600ab4a0fc1a6af3
Theovady Moutty <theomoutty@gmail.com>
2023-12-04 11:27:28 +0100

    fix: test

--- test/EnterpriseCrowdsale.ts
+++ test/EnterpriseCrowdsale.ts
@@ -26,7 +26,7 @@ describe("EnterpriseCrowdsale", function () {
         expect(buyerBalance).to.equal(1);
     });
 
-    it("should allow purchasing tokens if Not enough tokens available", async function () {
+    it("should not allow purchasing tokens if not enough tokens available", async function () {
         const [owner, otherAccount] = await ethers.getSigners();
 
         const EnterpriseToken = await ethers.getContractFactory("EnterpriseToken");
@@ -41,11 +41,66 @@ describe("EnterpriseCrowdsale", function () {
         10000 
         );
     
-        await enterpriseToken.transfer(await enterpriseCrowdsale.getAddress(), 200);    
+        await enterpriseToken.transfer(await enterpriseCrowdsale.getAddress(), 200); 
+   
         await owner.sendTransaction({ to: otherAccount.address, value: ethers.parseEther("1000") });
-        
+
         await expect(
             enterpriseCrowdsale.connect(otherAccount).purchaseTokens(400, { value: ethers.parseEther("200") })
         ).to.be.revertedWith('Not enough tokens available');   
+
+        const buyerBalance = await enterpriseToken.balanceOf(otherAccount.address);
+        expect(buyerBalance).to.equal(0);
     });
+
+    it("should allow purchasing tokens during the active sale", async function () {
+        const [owner, otherAccount] = await ethers.getSigners();
+        const EnterpriseToken = await ethers.getContractFactory("EnterpriseToken");
+        const enterpriseToken = await EnterpriseToken.deploy(100);
+        await enterpriseToken.waitForDeployment();
+    
+        const Crowdsale = await ethers.getContractFactory("EnterpriseCrowdsale");
+        const crowdsale = await Crowdsale.deploy(
+          await enterpriseToken.getAddress(),
+          1,
+          100,
+          10000
+        );
+    
+        await ethers.provider.send("evm_increaseTime", [5000]);
+        await ethers.provider.send("evm_mine", []);
+    
+        await enterpriseToken.transfer(await crowdsale.getAddress(), 100);
+
+        await owner.sendTransaction({ to: otherAccount.address, value: ethers.parseEther("1000") });
+        await crowdsale.connect(otherAccount).purchaseTokens(10, { value: ethers.parseEther("200") });
+    
+        const buyerBalance = await enterpriseToken.balanceOf(otherAccount.address);
+        expect(buyerBalance).to.equal(10);
+      });
+    
+      it("should not allow purchasing tokens after the sale has ended", async function () {
+        const [owner, otherAccount] = await ethers.getSigners();
+        const EnterpriseToken = await ethers.getContractFactory("EnterpriseToken");
+        const enterpriseToken = await EnterpriseToken.deploy(100);
+        await enterpriseToken.waitForDeployment();
+    
+        const Crowdsale = await ethers.getContractFactory("EnterpriseCrowdsale");
+        const crowdsale = await Crowdsale.deploy(
+          await enterpriseToken.getAddress(),
+          1,
+          100,
+          1000
+        );
+        await enterpriseToken.transfer(await crowdsale.getAddress(), 100);
+    
+        await ethers.provider.send("evm_increaseTime", [1000]);
+        await ethers.provider.send("evm_mine", []);
+    
+        await expect(
+          crowdsale.connect(otherAccount).purchaseTokens(5, { value: ethers.parseEther("200") })
+        ).to.be.revertedWith("Sale has ended");
+    });
+    
+    
 });
